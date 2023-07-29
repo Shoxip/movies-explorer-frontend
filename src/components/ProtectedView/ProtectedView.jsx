eefdcc88c5db54ae7ee7b5be90b551a4c0eb4e03
Roudy <savokmms1@mail.ru>
2023-07-29 14:29:05 +0300

    chore: я не помню что тут делал, но оно полезное

--- src/components/ProtectedView/ProtectedView.jsx
+++ src/components/ProtectedView/ProtectedView.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './ProtectedView.css'
-const ProtectedView = ({children, isLoggedIn, isAuthNeeded}) => {
-    return (
-      !isLoggedIn || !isAuthNeeded
-        ?
-          (<div className={'center'}>
-            <h1>У вас нет доступа к этой страничке</h1>
-          </div>)
-        : <>{children}</>
-
-
-    );
+const ProtectedView = ({children, isLoggedIn, isPublicPage}) => {
+  return isPublicPage ? (
+    isLoggedIn ? (
+      <>{children}</>
+    ) : (
+      <div className={'center'}>
+        <h1>У вас нет доступа к этой страничке</h1>
+      </div>
+    )
+  ) : (
+    <>{children}</>
+  );
 };
 
 export default ProtectedView;
