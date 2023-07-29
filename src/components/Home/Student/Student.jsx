607396599523acb13c111c0b8096005a879efecf
DISSMIND\DissM <dissmind@gmail.com>
2023-07-29 10:47:49 +0300

    fix student component

--- src/components/Home/Student/Student.jsx
+++ src/components/Home/Student/Student.jsx
@@ -6,26 +6,26 @@ export default function Student() {
   return (
     <LayoutBlock
       headersTitle={'Студент'}
-      className={'about-me'}
-      id={'about-me'}
+      className={'student'}
+      id={'student'}
     >
-      <div className='about-me__info'>
-        <div className='about-me__info-text'>
-          <h3 className='about-me__name'>
+      <div className='student__info'>
+        <div className='student__info-text'>
+          <h3 className='student__name'>
             Анна
           </h3>
 
-          <p className='about-me__job'>
+          <p className='student__job'>
             Фронтенд-разработчик, 30 лет
           </p>
 
-          <p className='about-me__description'>
+          <p className='student__description'>
             Я родилась и живу в Санкт-Петербурге, закончила факультет экономики и финансов
             в СПбГУ. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начала кодить. С 2015 года работала в компании «СКБ Контур». После того, как прошла курс по веб-разработке, начала заниматься фриланс-заказами и ушла с постоянной работы.
           </p>
 
           <a
-            className='link about-me__link'
+            className='link student__link'
             href='https://github.com/SeikoAnna'
             target='_blank'
             rel='noreferrer'
@@ -33,7 +33,7 @@ export default function Student() {
             Github
           </a>
         </div>
-        <img className='about-me__image' src={photo} alt='Фото цветы' />
+        <img className='student__image' src={photo} alt='Фото цветы' />
       </div>
     </LayoutBlock>
   );
