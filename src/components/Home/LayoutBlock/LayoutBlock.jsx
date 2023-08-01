648dd59ba507927a91b518a2371bb05a1b620d57
DISSMIND\DissM <dissmind@gmail.com>
2023-08-01 15:01:33 +0300

    fix style after fix BEM

--- src/components/Home/LayoutBlock/LayoutBlock.jsx
+++ src/components/Home/LayoutBlock/LayoutBlock.jsx
@@ -3,8 +3,8 @@ import './LayoutBlock.css'
 
 export default function LayoutBlock({headersTitle, className, id, children}) {
   return (
-    <section className={className}>
-      <h2 className={className + '__title'}>{headersTitle}</h2>
+    <section className={className + ' layout-block'}>
+      <h2 className={className + '__title' + ' layout-block__title'}>{headersTitle}</h2>
 
       <div className={className + "__content"}>
         {children}
